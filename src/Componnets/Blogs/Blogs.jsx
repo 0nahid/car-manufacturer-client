7e7e7422f0dcbffb6567113e28a09925efa66eea
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-08-06 15:46:11 +0600

    optimize some section

--- src/Componnets/Blogs/Blogs.jsx
+++ src/Componnets/Blogs/Blogs.jsx
@@ -1,40 +1,58 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Blogs() {
-    const [blogs, setBlogs] = useState([]);
-    useEffect(() => {
-        axios(`https://car-parts-bangladesh.herokuapp.com/api/blogs`)
-            .then(res => {
-                setBlogs(res.data);
-            })
-    }, [])
-    // console.log(blogs);
-    return (
-        <section className="text-gray-600 body-font">
-            <div className="container px-5 py-24 mx-auto">
-                <div className="flex flex-wrap -m-4">
-                    {
-                        blogs?.map((service, index) => (
-                            <>
-                                <div className="p-4 lg:w-1/3">
-                                    <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-                                        <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">{service?.question}</h1>
-                                        <p className="leading-relaxed mb-3">{service?.answer.slice(0, 100)} </p>
-                                        <Link to={`/blogs/${service?._id}`} className="text-pink-500 inline-flex items-center">Learn More
-                                            <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                                <path d="M5 12h14"></path>
-                                                <path d="M12 5l7 7-7 7"></path>
-                                            </svg>
-                                        </Link>
-                                    </div>
-                                </div>
-                            </>
-                        ))
-                    }
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    axios(`https://car-parts-bangladesh.herokuapp.com/api/blogs`).then(
+      (res) => {
+        setBlogs(res.data);
+      }
+    );
+  }, []);
+  // console.log(blogs);
+  return (
+    <section className="text-gray-600 body-font">
+      <h1 className="text-center text-primary font-bold text-3xl mt-10 mb-10">
+        Blogs
+      </h1>
+      <div className="container px-5 py-10 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          {blogs?.map((service, index) => (
+            <>
+              <div className="p-4 lg:w-1/3">
+                <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+                  <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+                    {service?.question}
+                  </h1>
+                  <p className="leading-relaxed mb-3">
+                    {service?.answer.slice(0, 100)}{" "}
+                  </p>
+                  <Link
+                    to={`/blogs/${service?._id}`}
+                    className="text-pink-500 inline-flex items-center"
+                  >
+                    Learn More
+                    <svg
+                      className="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M5 12h14"></path>
+                      <path d="M12 5l7 7-7 7"></path>
+                    </svg>
+                  </Link>
                 </div>
-            </div>
-        </section>
-    )
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/Componnets/Shop/ShopData.jsx
+++ src/Componnets/Shop/ShopData.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ShopData({ service }) {
-    const { productName, image, _id, price, availableQty } = service;
-    return (
-        <div className="card card-compact w-full bg-slate-50/60 backdrop-blur-2xl transition-colors duration-500 shadow-xl">
-            <figure><img src={image} className="h-52" alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">
-                    {productName}
-                    <div className="badge badge-neural">NEW</div>
-                </h2>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">{availableQty} left</div>
-                    <div className="badge ">{price}$ only</div>
-                </div>
-                <div className="card-actions justify-end">
-                    <Link to={`/services/${_id}`} className="btn btn-primary">Details</Link>
-                </div>
-            </div>
+  const { productName, image, _id, price, availableQty } = service;
+  return (
+    <div className="card card-compact w-full bg-base-100 backdrop-blur-2xl transition-colors duration-500 shadow-xl">
+      <figure>
+        <img src={image} className="h-52" alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {productName}
+          <div className="badge badge-neural">NEW</div>
+        </h2>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">{availableQty} left</div>
+          <div className="badge ">{price}$ only</div>
         </div>
-    )
+        <div className="card-actions justify-end">
+          <Link to={`/services/${_id}`} className="btn btn-primary">
+            Details
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
